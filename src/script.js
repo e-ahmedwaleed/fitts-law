800218f2eeaa8d3cfb7bba3862c9b68e145fbf4b
EngAhmedWaleed <ahmedwaleed@csed22.com>
2020-11-05 19:56:50 +0200

    better structure

---
+++ src/script.js
@@ -0,0 +1,22 @@
+var isClicked = [false, false, false];
+
+function lockButton(button, index) {
+    if (isClicked[index]) return;
+    isClicked[index] = true;
+
+    button.childNodes[1].style.left = 0;
+    button.childNodes[3].text = "";
+    button.style.pointerEvents = "none";
+    button.style.backgroundColor = "#bfc0c0";
+
+    if (isClicked[-1] && isClicked[0] && isClicked[1])
+        sleep(500).then(() => {
+            location.reload();
+        });
+
+}
+
+// https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
