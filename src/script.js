7a271b299ad7631d3f1a8c282e5b7d74d6a45f60
EngAhmedWaleed <ahmedwaleed@csed22.com>
2020-11-06 15:17:15 +0200

    added color picker, and before cleaning

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
 var isClicked = [false, false, false];
+var isLoaded = [false, false, false];
+
 
 function lockButton(button, index) {
     if (isClicked[index]) return;
@@ -14,6 +16,38 @@ function lockButton(button, index) {
             location.reload();
         });
 
+}
+
+function randomizePositions() {
+    sleep(500).then(() => {
+        var i = 0;
+        for (const button of document.getElementsByClassName("button")) {
+
+            var max_width = window.innerWidth;
+            var min_width = max_width * i / 3;
+            console.log(max_width + ", " + min_width)
+
+            var max_height = window.innerHeight - (button.offsetHeight * 2);
+            console.log(max_height)
+
+            var randomY = Math.floor(Math.random() * max_height) - 20;
+            button.style.top = Math.max(randomY, 20) + "px";
+            var randomX = min_width - (button.offsetWidth + 20) + Math.floor(Math.random() * (max_width / 3));
+            button.style.left = Math.max(randomX, min_width + 20) + "px";
+            button.style.visibility = "visible";
+
+            console.log(button)
+            console.log(button.offsetWidth)
+
+            i++;
+        }
+    });
+}
+
+function randomizeButton(button, index) {
+    button.style.top = Math.floor(Math.random() * window.innerWidth) + "px";
+    button.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+    button.style.visibility = "visible";
 }
 
 function timeDestroy(gif) {
