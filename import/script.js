0cea4d41c4ffc8a7f76f1a99c8a8b24e8b806322
EngAhmedWaleed <ahmedwaleed@csed22.com>
2020-11-07 11:49:26 +0200

    basic functionality perfected

--- import/script.js
+++ import/script.js
@@ -1,18 +1,27 @@
+var startTime;
+var midTime;
+var endTime;
 var isClicked = [false, false, false];
-var isLoaded = [false, false, false];
-
 
 function lockButton(button, index) {
     if (isClicked[index]) return;
 
     switch (index) {
+        case 0: // if center, start timer and remove hand if existed.
+            startTime = new Date();
+            var hand = document.getElementsByClassName("gif")[0];
+            if (hand != null)
+                hand.parentNode.removeChild(hand);
+            break;
         case 1: // if right, check center.
             if (!isClicked[0])
                 return;
+            midTime = new Date();
             break;
         case 2: // if left, check center and right.
             if (!(isClicked[0] && isClicked[1]))
                 return;
+            endTime = new Date();
             break;
     }
 
@@ -24,33 +33,69 @@ function lockButton(button, index) {
     button.style.pointerEvents = "none";
     switch (index) {
         case 0: // if center, active right.
-            document.getElementsByClassName("button")[2].style.pointerEvents = "all";
+            document.getElementsByClassName("button")[1].style.pointerEvents = "all";
             break;
         case 1: // if right, active left.
-            document.getElementsByClassName("button")[0].style.pointerEvents = "all";
+            document.getElementsByClassName("button")[2].style.pointerEvents = "all";
             break;
     }
 
     if (isClicked[0] && isClicked[1] && isClicked[2])
         sleep(500).then(() => {
-            location.reload();
+            console.log(window.innerHeight + "," + window.innerWidth);
+            // https://stackoverflow.com/questions/2024198/how-many-seconds-between-two-dates
+            console.log((midTime.getTime() - startTime.getTime()) + "," + (endTime.getTime() - midTime.getTime()) + "," + (endTime.getTime() - startTime.getTime()));
+
+            for (const button of document.getElementsByClassName("button")) {
+                console.log(button.style.top + "," + button.style.left + "," + button.offsetHeight + "," + button.offsetWidth);
+            }
+
+            randomizeButtons();
         });
 
 }
 
-function randomizeButtons() {
+function unlockButton(button, index) {
+    // https://stackoverflow.com/questions/3506050/how-to-reset-the-style-properties-to-their-css-defaults-in-javascript
+    button.childNodes[1].attributeStyleMap.delete('top');
+    button.childNodes[1].attributeStyleMap.delete('left');
+
+    switch (index) {
+        case 0:
+            button.childNodes[3].text = "1";
+            break;
+        case 1:
+            button.childNodes[3].text = "2";
+            break;
+        case 2:
+            button.childNodes[3].text = "3";
+            break;
+    }
+}
+
+function initializeButtons() {
     initializeColors();
+    randomizeButtons();
+}
+
+function randomizeButtons() {
     var i = 0;
-    for (const button of document.getElementsByClassName("button")) {
+    var buttons = document.getElementsByClassName("button")
+    for (const button of buttons) {
+        isClicked[i] = false;
         randomizeSize(button, i);
         randomizePosition(button, i);
+        unlockButton(button, i);
         i++;
     }
-    for (const button of document.getElementsByClassName("button")) {
+    for (const button of buttons) {
         button.style.visibility = "visible";
         button.children[1].style.visibility = "visible";
     }
-    trackStartPoint();
+    buttons[0].style.pointerEvents = "all";
+    sleep(100).then(() => {
+        trackStartPoint();
+    });
 }
 
 function initializeColors() {
@@ -62,7 +107,7 @@ function initializeColors() {
 function randomizeSize(button, index) {
 
     var max_height = window.innerHeight / 3;
-    var max_width = (window.innerWidth / 6) / (index == 1 ? 2 : 1);
+    var max_width = (window.innerWidth / 6) / (index == 0 ? 2 : 1);
 
     var randomHeight = Math.floor(Math.random() * max_height);
     button.style.height = Math.max(randomHeight, 25) + "px";
@@ -70,13 +115,15 @@ function randomizeSize(button, index) {
     var randomWidth = Math.floor(Math.random() * max_width);
     button.style.width = Math.max(randomWidth, 25) + "px";
 
-    if (index == 1)
+    if (index == 0)
         button.style.height = button.style.width;
 
     button.children[1].style.fontSize = 0.8 * Math.min(button.offsetHeight, button.offsetWidth);
 }
 
 function randomizePosition(button, index) {
+
+    index = (index + 1) % 3;
     var max_width = window.innerWidth;
     var min_width = max_width * index / 3;
 
@@ -92,11 +139,13 @@ function randomizePosition(button, index) {
 function trackStartPoint() {
 
     var hand = document.createElement("IMG");
-    hand.setAttribute("id", "hand_click");
     hand.setAttribute("class", "gif");
-    hand.setAttribute("src", "media/hand_clicking.gif");
     hand.setAttribute("style", "visibility: hidden;");
     hand.setAttribute("onload", "timeDestroy(this)");
+
+    //https://stackoverflow.com/questions/58589941/pure-javascript-remove-element-on-mouseover
+    hand.addEventListener("mouseover", destroyOnHover);
+
     document.body.appendChild(hand);
 
     var button = document.getElementById("center-button");
@@ -108,12 +157,20 @@ function trackStartPoint() {
 
     hand.style.left = x;
     hand.style.top = y;
+
+    // https://stackoverflow.com/questions/19831319/restart-a-gif-animation-without-reloading-the-file/24810221
+    hand.src = "media/hand_clicking.gif";
     hand.style.visibility = "visible";
 }
 
+function destroyOnHover() {
+    event.target.parentNode.removeChild(event.target);
+}
+
 function timeDestroy(gif) {
-    sleep(1800).then(() => {
-        gif.parentNode.removeChild(gif);
+    sleep(1650).then(() => {
+        if (gif.parentNode != null)
+            gif.parentNode.removeChild(gif);
     });
 }
 
